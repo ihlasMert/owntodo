579e425ae5352ede7522a73b904efecd7b681ce4
İhlas Mert <88083556+ihlasMert@users.noreply.github.com>
2022-05-19 21:47:24 +0300

    added form

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,27 @@
-
+import  React, {useState} from "react"
+import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  //function click handler(){}
+  const [title, setTitle] = useState(props.title);
+
+
+  const clickHandler =() => {
+  setTitle("updated");
+    console.log(title);
+
+  }
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price"> ${props.amount}</div>
       </div>
-    </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
   );
 };
 
